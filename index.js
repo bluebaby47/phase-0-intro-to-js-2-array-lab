0d63106bd2cf9045a1eb1f2ca6da850a2100591f
kevin ikahu <kevinikahu4@gmail.com>
2023-03-04 08:54:50 -0800

    array test

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Write your solution here!
+const cats = ['Milo', 'Otis', 'Garfield']
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+
+function destructivelyPrependCat(name){
+    cats.splice(0, 0, name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.splice(0, 1)
+}
+
+function appendCat(name){
+    const array= [...cats]
+    array.push(name)
+    return array
+}
+
+function prependCat(name){
+    const array= [...cats]
+    array.splice(0, 0, name)
+    return array
+}
+
+function removeLastCat(){
+    return cats.slice(0, cats.length-1)
+}
+
+function removeFirstCat(){
+    return cats.slice(1)
+}
